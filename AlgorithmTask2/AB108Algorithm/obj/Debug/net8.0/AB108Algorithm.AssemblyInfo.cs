c70129ef0f27110ec1b7bdf331090a672fa5bd2e
AliyevVilayat <77es4pa@code.edu.az>
2024-10-09 14:38:40 +0400

    Algorithm Task 2 updated

--- AlgorithmTask2/AB108Algorithm/obj/Debug/net8.0/AB108Algorithm.AssemblyInfo.cs
+++ AlgorithmTask2/AB108Algorithm/obj/Debug/net8.0/AB108Algorithm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AB108Algorithm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f09c2e0d286e15ea48c2c4148585ffcddaef63d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AB108Algorithm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AB108Algorithm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
