2ab69635de3d3e7f265ffd2ef916f48ec8741563
AliyevVilayat <77es4pa@code.edu.az>
2024-10-12 13:26:01 +0400

    Method Tasks added

---
+++ AB108MEthod/AB108MEthod/Program.cs
@@ -0,0 +1,79 @@
+﻿#region Notes
+
+//Void - Əgər method geriyə heç bir dəyər qaytarmayacaqsa(yəni return etməyəcəksə) bu zaman method-un geri dönüş tipi void olmalıdır.
+
+#endregion
+
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+
+    }
+
+    #region CustomToLower()
+
+    public static string CustomToLower(string str)
+    {
+        //Custom ToLower(); Elə bir method yazın ki göndərilən string-i kiçik hərflərlə yazılmış versiyasını geriyə qaytarsın.
+
+        char newSymb = '\0';
+        string lowerStr = ""; //=> string.Empty;
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] >= 65 && str[i] <= 90)
+            {
+                newSymb = (char)(str[i] + 32);
+                lowerStr += newSymb;
+            }
+            else
+            {
+                lowerStr += str[i];
+            }
+        }
+
+        return lowerStr;
+    }
+
+    #endregion
+
+
+    #region CustomIndexOf()
+
+    //Custom IndexOf(); Elə bir method yazın ki göndərilən array daxilində göndərilmiş olan dəyəri axtarsın və həmin index-i qaytarsın. Əgər yoxdursa geriyə -1 qaytarsın.
+
+    public static int CustomIndexOf(int[] arr, int searchedVal)
+    {
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == searchedVal)
+            {
+                return i;
+            }
+
+        }
+
+        return -1;
+
+    }
+
+    #endregion
+
+
+    #region HomeWork
+
+
+    //Custom ToUpper(); Elə bir method yazın ki göndərilən string-i böyük hərflərlə yazılmış versiyasını geriyə qaytarsın.
+    //Custom CleanStr(); Elə bir method yazın ki göndərilən string-in daxilində yer alan ƏLAVƏ simvolları təmizləsin(yəni sadəcə geriyə boşluqlar və sözlər qayıtsın.
+
+    #endregion
+
+
+
+}
+
