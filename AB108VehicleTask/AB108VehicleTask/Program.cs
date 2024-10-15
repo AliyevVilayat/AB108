be3dd9f9b14894615e17149dad89fb3a8826a0e3
AliyevVilayat <77es4pa@code.edu.az>
2024-10-15 23:01:45 +0400

    Vehicle Task added

---
+++ AB108VehicleTask/AB108VehicleTask/Program.cs
@@ -0,0 +1,20 @@
+﻿
+#region Notes
+
+//Constructor - Object instance alınan zaman(yəni obbyekt new-lanan zaman başqa bir dildə desək Class-dan obyekt yaranan zaman) işə düşən return type-ı OLMAYAN bir kod blokudur.
+
+//Parameterless - Default - Boş Constructor.
+
+//Constructor bir başa olaraq yalnız 1 constructor-u call edə bilər.
+
+//This keyword-ü işlətdiyimiz class-ın obyektini işarə edir.
+#endregion
+
+
+using AB108VehicleTask;
+
+
+Car myNewCar = new("M5", "BMW", 65, 0.35, 2005);
+
+myNewCar.currentFuel = 5;
+myNewCar.Drive(100);
---
+++ AB108VehicleTask/AB108VehicleTask/Vehicle.cs
@@ -0,0 +1,59 @@
+﻿
+//Gerəksiz olan Namespace-lər CTRL + R + G vasitəsi ilə silinir.
+namespace AB108VehicleTask;
+
+public class Vehicle
+{
+    public string color;
+    public int year;
+
+
+    public Vehicle(int year)
+    {
+        this.year = year;
+        Console.WriteLine(year);
+    }
+}
+
+public class Car : Vehicle
+{
+    public string brand;
+    public string model;
+    public double fuelCapacity;
+    public double fuelForOneKm;
+    public double currentFuel;
+
+    public Car(string model, string brand, double fuelCapacity, double fuelForOneKm, int year) : base(year)
+    {
+        this.brand = brand;
+        this.model = model;
+        this.fuelCapacity = fuelCapacity;
+        this.fuelForOneKm = fuelForOneKm;
+    }
+
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Car brand: {brand}");
+    }
+
+    public void Drive(double distance)
+    {
+
+        double totalDistanceWithCurrentFuel = currentFuel / fuelForOneKm;
+        if (totalDistanceWithCurrentFuel - distance >= 0)
+        {
+            currentFuel = (totalDistanceWithCurrentFuel - distance) * fuelForOneKm;
+            Console.WriteLine($"{distance}KM yol qət edildi. Qalan yanacaq:{currentFuel}l");
+        }
+        else
+        {
+            Console.WriteLine($"Hazirdaki benzinle({currentFuel}) yalnızca {currentFuel / fuelForOneKm} KM yol qət edə bilərsiniz. Əgər hələ də bu yolu getmək istəyirsinizsə {(distance - totalDistanceWithCurrentFuel) * fuelForOneKm}l benzinə ehtiyyacınız var");
+
+            Console.WriteLine("Daççik özünü öldürür, zəhmət olmasa benzin vur.");
+        }
+
+    }
+
+
+}
