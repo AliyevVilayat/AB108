d6f4ba6d514edf074a14fe90c382c24e3614a940
ulviabdullazada <ulvi.abdullazda@code.edu.az>
2024-10-17 15:26:46 +0400

    Weapon task added|

---
+++ WeaponTask/WeaponTask/Program.cs
@@ -0,0 +1,158 @@
+﻿using System.IO;
+
+namespace WeaponTask;
+
+#region ForMe
+
+// - Inheritance
+
+// - Encapsulation
+
+// - Access Modifiers
+
+#endregion
+
+#region Notes
+
+/*
+ Break keyword - Müəyyən şərt daxilində Loop-u və Switch Statement-i dəyandırmaq üçündür.
+ Return keyword - Müəyyən şərt daxilində Method-u dəyandırmaq üçündür və ya Method-un return type-a uyğun olaraq geriyə dəyər döndürmək üçündür.
+
+Default - Switch statement, DEFAULT KEYWORD OLMADAN da yazıla bilər.
+
+ Switch Case-in duzgun islemesi ucun case daxilinde hec olmasa asagidakilardan biri yazilmalidir:
+    1.Break
+    2.Goto case <value>
+    3.Return
+    4.Case daxili bos buraxilmalidir
+
+    Extra. Throw Exception
+ */
+
+
+
+#endregion
+
+#region Task
+
+/*
+ Weapon deyə class yaradılır. +
+
+Darağın güllə tutumu, Daraqdakı güllə sayı, Darağın boşalma saniyəsi, 
+atış modu (təkli və ya avtomatik) propertyləri verilir. +
+
+Shoot() - metodu hər çağırıldıqda 1 güllə atır(Ekranda bunu bildir) +
+
+Fire() - metodu daraqdakı güllələr hamısı atəşlənir və neçə saniyəyə bitdiyi qeyd olunur. +
+
+GetRemainBulletCount() - darağın tam dolması üçün lazım olan güllə sayını qaytarır.+
+
+Reload() - darağı yenidən doldurur.+
+
+ChangeFireMode() - Atış modunu dəyişir.+
+
+Bütün məlumatları doldurmadan obyekt yaratmaq olmasın.
+0 - İnformasiya almaq üçün
+1 - Shoot metodu üçün
+2 - Fire metodu üçün
+3 - GetRemainBulletCount metodu üçün
+4 - Reload metodu üçün
+5 - ChangeFireMode metodu üçün
+6 - Proqramdan dayandırmaq üçün
+qısayoldur.
+
+ */
+
+#endregion
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        //Constructor ilə        
+        Weapon weapon = new Weapon(32, 32, 3.6f, "Auto");
+
+        Console.WriteLine(
+
+                   """ 
+            Bütün məlumatları doldurmadan obyekt yaratmaq olmasın.
+            0 - İnformasiya almaq üçün
+            1 - Shoot metodu üçün
+            2 - Fire metodu üçün
+            3 - GetRemainBulletCount metodu üçün
+            4 - Reload metodu üçün
+            5 - ChangeFireMode metodu üçün
+            6 - Proqramdan dayandırmaq üçün qısayoldur.
+            """
+
+                   );
+
+        int code = 0;
+        bool isContinue = true;
+        while (isContinue)
+        {
+
+            Console.Write("\nZehmet olmasa 0 ve 6 araliginda deyer gonderin:");
+            code = Convert.ToInt32(Console.ReadLine());  // => int.Parse();
+
+            switch (code)
+            {
+                case 0:
+
+                    Console.WriteLine("Informasiya verildi");
+                    break;
+
+                case 1:
+                    {
+                        weapon.Shoot();
+                        break;
+                    }
+
+                case 2:
+                    weapon.Fire();
+                    break;
+
+                case 3:
+                    Console.WriteLine(weapon.GetRemainBulletsCount()); 
+                    break;
+
+                case 4:
+                    weapon.Reload();
+                    break;
+
+                case 5:
+                    weapon.ChangeMod();
+                    break;
+
+                case 6:
+                    Console.WriteLine("Proqram deyandirildi");
+                    isContinue = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Duzgun code daxil edilmedi!\n");
+                    Console.WriteLine(
+
+           """ 
+            Bütün məlumatları doldurmadan obyekt yaratmaq olmasın.
+            0 - İnformasiya almaq üçün
+            1 - Shoot metodu üçün
+            2 - Fire metodu üçün
+            3 - GetRemainBulletCount metodu üçün
+            4 - Reload metodu üçün
+            5 - ChangeFireMode metodu üçün
+            6 - Proqramdan dayandırmaq üçün qısayoldur.
+            """
+
+           );
+                    break;
+
+            }
+        }
+
+
+
+    }
+}
---
+++ WeaponTask/WeaponTask/Weapon.cs
@@ -0,0 +1,135 @@
+﻿namespace WeaponTask;
+
+public class Weapon
+{
+    public int BulletCapacity { get; set; }
+    public int CurrentBulletsCount { get; set; }
+    public float DischargeTime { get; set; }
+    public string FireMod { get; set; }
+
+    public Weapon()
+    {
+
+    }
+
+    public Weapon(int bulletCapacity, int currentBulletsCount, float dischargeTime, string fireMod)
+    {
+        BulletCapacity = bulletCapacity;
+        CurrentBulletsCount = currentBulletsCount;
+        DischargeTime = dischargeTime;
+        FireMod = fireMod;
+    }
+
+    public void Shoot()
+    {
+        if (CurrentBulletsCount > 0)
+        {
+            CurrentBulletsCount--; // => CurrentBulletsCount = CurrentBulletsCount - 1;   => CurrentBulletsCount -=1;
+            Console.WriteLine("Ates edildi!!!");
+            Console.WriteLine($"Daraqda qalan gulle sayi {CurrentBulletsCount}/{BulletCapacity}");
+        }
+        else
+        {
+            Console.WriteLine("Hal-hazirda daraqda gulle yoxdur!!!");
+            Console.WriteLine("Avtomatik olaraq daraq deyisdirilecek!");
+            Reload();
+
+        }
+
+    }
+
+    public void Fire()
+    {
+        if (CurrentBulletsCount > 0)
+        {
+            if (FireMod == "Auto")
+            {
+                Console.WriteLine("Ates basladi!!!");
+
+                float spentTime = (CurrentBulletsCount * DischargeTime) / BulletCapacity;
+
+                Thread.Sleep((int)spentTime * 500);
+
+                Console.WriteLine("Ates davam edir...");
+
+                Thread.Sleep((int)spentTime * 500);  //Sadece proqrami spentTime qeder deyandiracaq
+
+                Console.WriteLine("Ates bitdi!");
+
+                Console.WriteLine($"{CurrentBulletsCount} gulle istifade edildi. Ates {spentTime} saniye davam etdi");
+
+                CurrentBulletsCount = 0;
+
+            }
+            else
+            {
+                Shoot();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Hal-hazirda daraqda gulle yoxdur!!!");
+            Console.WriteLine("Daragi deyismek lazimdir!!!");
+
+            Console.Write("Daragi deyisdirmek isteyirsiniz mi?(y/n):");
+            string code = default;
+            bool isContinue = true;
+            while (isContinue)
+            {
+                code = Console.ReadLine();
+                if (code == "y")
+                {
+                    Reload();
+                    isContinue = false;
+                }
+                else if (code == "n")
+                {
+                    Console.WriteLine("Silahin daragi bos olaraq qalir!");
+                    isContinue = false;
+                }
+                else
+                {
+                    Console.WriteLine("Duzgun code daxil edilmedi!!!");
+                    Console.Write("Daragi deyisdirmek isteyirsiniz mi?(y/n):");
+                }
+            }
+
+        }
+    }
+
+    public int GetRemainBulletsCount()
+    {
+        int remainBulletsCount = BulletCapacity - CurrentBulletsCount;
+        return remainBulletsCount;
+    }
+
+    public void Reload()
+    {
+        if (CurrentBulletsCount != BulletCapacity)
+        {
+            Console.WriteLine("Daraq deyisdirilir...");
+            Thread.Sleep(3000);
+
+            Console.WriteLine($"Daraq deyisdirildi.");
+            CurrentBulletsCount = BulletCapacity;
+        }
+        else
+        {
+            Console.WriteLine("Bos bosuna daraq xercleme!!!");
+        }
+    }
+
+    public void ChangeMod()
+    {
+        if (FireMod == "Auto")
+        {
+            FireMod = "Single";
+        }
+        else
+        {
+            FireMod = "Auto";
+        }
+
+        Console.WriteLine($"Atis modu {FireMod} olaraq deyisdirildi");
+    }
+}
